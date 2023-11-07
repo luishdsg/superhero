22ecf7ff2b636c76cf44c85a654bb326bec958a3
luishdsg <65797541+luishdsg@users.noreply.github.com>
2023-11-07 02:53:40 -0300

    Update getSuperHeroesData.tsx

--- src/services/getSuperHeroesData.tsx
+++ src/services/getSuperHeroesData.tsx
@@ -4,6 +4,8 @@ import {SuperHeroInterface} from '../interface/superhero-interface';
 
 export const SuperHeroesData = async (): Promise<SuperHeroInterface[]> => {
   try {
+    // infelizmente troqueia  API pois a outra estava dando algusn erros
+    // http://homologacao3.azapfy.com.br/api/ps/metahumans
     const response = await axios.get('https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/all.json');
     return response.data;
   } catch (error) {
